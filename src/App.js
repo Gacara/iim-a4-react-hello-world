341fd18525adc569e2c6f9564318eabeb772213c
Romain Goulet <romain.goulet@edu.devinci.fr>
2020-09-30 11:38:25 +0200

    init commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Tu parles avec une Anissa mais moi j'm'appelle Wejdene ! 
+          <span role="img">
+          👼🏻
+            </span>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
